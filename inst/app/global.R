e150fec9e063303e049a87d8bd43b95d3b8cf57c
Simon Garnier <simon.garnier@gmail.com>
2015-01-08 15:45:48 -0500

    Moved bsNavDropDown2 out of global.R and to its own function file.

--- inst/app/global.R
+++ inst/app/global.R
@@ -3,37 +3,6 @@ library(shiny)
 ### ###
 
 
-### Define custom functions ###
-bsNavDropDown2 <- function (inputId, label, choices, ids, selected = "") {
-  if (!inherits(label, "shiny.tag")) 
-    label <- HTML(label)
-  
-  choices <- lapply(choices, FUN = function(choice) {
-    if (length(choice) == 1) {
-      if (choice[1] == "~sep~") {
-        tags$li(tags$hr(class = "hr_menu"))
-      } else {
-        tags$li(tags$a(tabindex = "-1", 
-                       href = "#", HTML(choice), class="action-button"))
-      }
-    } else {
-      if (choice[1] == "~sep~") {
-        tags$li(tags$hr(class = "hr_menu"))
-      } else {
-        tags$li(tags$a(tabindex = "-1", 
-                       href = "#", id = choice[2], 
-                       HTML(choice[1]), class="action-button"))
-      }
-    }
-  })
-  
-  tags$li(id = inputId, class = "dropdown sbs-dropdown", `data-value` = selected, 
-          tags$a(href = "#", class = "dropdown-toggle", `data-toggle` = "dropdown", 
-                 label, tags$b(class = "caret")), tags$ul(class = "dropdown-menu", choices))
-}
-### ###
-
-
 ### Set options ###
 .Options$editR$editor_font_size <- ifelse(is.null(.Options$editR$editor_font_size), 
                                           14, .Options$editR$editor_font_size)
