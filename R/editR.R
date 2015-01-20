cc88d9e495196a28109f1a613ca8b44d6cb81894
Simon Garnier <simon.garnier@gmail.com>
2015-01-20 16:14:50 -0500

    Converted editR documentation to Roxygen format.

--- R/editR.R
+++ R/editR.R
@@ -1,3 +1,27 @@
+#' Edit Rmarkdown files with instant preview
+#'
+#' This function create a GUI to edit \code{\link[<rmarkdown>:<rmarkdown>-package]{Rmarkdown}} 
+#' documents and display an instant preview of the document as it is being 
+#' written. Rmarkdown documents can be rendered directly from the GUI. 
+#' 
+#' @param file The path (as a character string) to an existing .Rmd/.md file, or to a new .Rmd/.md file. If the file does not exist, it will be created when the GUI starts.
+#' 
+#' @return This function does not return anything.
+#' 
+#' @author Simon Garnier: \email{garnier@@njit.edu}, \link[https://twitter.com/sjmgarnier]{@@sjmgarnier}
+#' 
+#' @details If called from RStudio, it will open in the internal RStudio internet 
+#' browser. If called from a terminal, it will open in your default internet 
+#' browser. editR should work without any problem with the internal RStudio 
+#' internet browser, as well as with recent versions of most internet browsers. 
+#' It is likely to break with older versions.
+#' 
+#' @examples
+#' # Create and edit a new Rmarkdown document in the current working directory
+#' editR("example.Rmd")
+#' 
+#' @export
+#'
 editR <- function(file) {
   require(tools)
   require(shiny)
